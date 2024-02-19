7b56b053c7f681a3d26e5f4b94c450b61b140213
Sokolvek <vadim200608@yandex.ru>
2024-02-19 18:09:09 +0300

    upd

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ export default createRouter({
     history:createWebHashHistory(),
     routes:[
         {
-            path:"/portfolio",
+            path:"/",
             component:index
 
         }
